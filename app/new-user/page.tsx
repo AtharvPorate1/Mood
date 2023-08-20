ff8febc9fc5abe56d658a41b9d5b419ce74a2b89
[AtharvPorate1] <www.atharvporate254@gmail.com>
2023-08-20 16:08:53 +0530

    Added prisma ORM and wrote prisma Schema for database

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,3 +1,8 @@
+//This page is for syncing clerk with our database... So if we are not signed In We'll have to 
+//But If we are signed in, which we will be 100% of times, because this route is not public as seen in middleware.ts
+//SO what it does, whatever info is saved it saves in our database by our prisma schema
+
+
 
 
 const NewUser = () => {
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import { auth } from "@clerk/nextjs"
 import Link from "next/link"
 
-export default function Home() {
+
+export default async function Home() {
+
+  const {userId} = await auth()
+
+  let href = userId? '/journal':'new-user' 
+
+
   return (
     <div className='h-screen w-screen bg-black flex justify-center items-center text-white'>
       <div className='w-full max-w-[600px] mx-auto'>
@@ -10,7 +18,7 @@ export default function Home() {
       <p className='text-2xl text-white/60 mb-4'>The best app to track your mood</p>
 
       <div>
-        <Link href="/journal">
+        <Link href={href}>
         <button className='bg-blue-600 rounded text-white text-xl px-4 py-2'>Get Started</button>
         </Link>
       </div>
